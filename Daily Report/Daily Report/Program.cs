37a4cfae704efc8d8bbb68d0178af3cd8b74bd9b
DFaustin175 <deefaustin175@gmail.com>
2022-02-18 13:54:29 -0500

    Bool fix

--- Daily Report/Daily Report/Program.cs
+++ Daily Report/Daily Report/Program.cs
@@ -15,26 +15,18 @@ namespace Daily_Report
             Console.WriteLine("Your Course is " + yourCourse);
             Console.WriteLine("What page are you on?");
             string pageNum = Console.ReadLine();
-            Console.WriteLine("You are on page " + pageNum);
+            int page = Convert.ToInt16(pageNum);
+            Console.WriteLine("You are on page " + page);
             Console.WriteLine("Do you need help with anything? Please answer “true” or “false.”");
-            string answer = Console.ReadLine();
-            if (answer == "true")
-            {
-                Console.WriteLine("What topic do you need help with?");
-                string problem = Console.ReadLine();
-                Console.WriteLine("Ok someone will be able to help you with " + problem);
-            }
-            else if (answer == "false")
-            {
-                Console.WriteLine("Glad to hear it!");
-                
-            }
+            bool needsHelp = true;
+            Console.WriteLine(needsHelp);
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
             string exp = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
             string hoursStudied = Console.ReadLine();
+            int hours = Convert.ToInt16(hoursStudied);
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
         }
