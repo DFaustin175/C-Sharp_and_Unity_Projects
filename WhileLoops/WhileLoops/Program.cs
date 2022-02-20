078776f7e64d7f11d7539647c942141f263c4c71
DFaustin175 <deefaustin175@gmail.com>
2022-02-20 14:57:26 -0500

    Loops

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("While Satement");
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 12:
+                        Console.WriteLine("You guessed correctly");
+                        Console.ReadLine();
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            Console.WriteLine("Enter password");
+            string correctPassword = "sandwich";
+            string passwordAttempt = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Password incorrect");
+                passwordAttempt = Console.ReadLine();
+            }
+            while (correctPassword != passwordAttempt);
+            Console.WriteLine("Password accepted");
+            Console.ReadLine();
+        }
+    }
+}
