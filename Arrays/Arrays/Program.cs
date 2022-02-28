fc731d2ee4185284de438f05a985bcc83c8db03c
DFaustin175 <deefaustin175@gmail.com>
2022-02-28 13:00:16 -0500

    Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] names = { "Ryan", "Chris", "Brigid", "Sergi" };
+            int nameInput = int.Parse(Console.ReadLine());
+            if ( nameInput == 0 || nameInput == 1 || nameInput == 2 || nameInput == 3)
+            {
+                Console.WriteLine(names[nameInput]);
+            }
+            else 
+            {
+                Console.WriteLine("Error 404");
+            }
+
+            int[] nums = { 10, 32, 222, 790 };
+            int numsInput = int.Parse(Console.ReadLine());
+            if (numsInput == 0 || numsInput == 1 || numsInput == 2 || numsInput == 3)
+            {
+                Console.WriteLine(nums[numsInput]);
+            }
+            else
+            {
+                Console.WriteLine("Error 404");
+            }
+            Console.ReadLine();
+        }
+    }
+}
