76982fb623c77eef6ac29f1308ccda4c4ed52398
DFaustin175 <deefaustin175@gmail.com>
2022-03-07 14:27:49 -0500

    Methods

---
+++ Method Calling/Method Calling/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Calling
+{
+    internal class Class1
+    {
+        public static void Add(int x)
+        {
+            int A = x + 100;
+            Console.WriteLine(A);
+        }
+
+        public static void Mult(int z)
+        {
+            int B = z * 100;
+            Console.WriteLine(B);
+        }
+
+        public static void Sub(int c)
+        {
+            int C = 100 - c;
+            Console.WriteLine(C);
+        }
+    }
+}
---
+++ Method Calling/Method Calling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Calling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number:");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+            Class1.Add(numInput);
+            Class1.Mult(numInput);
+            Class1.Sub(numInput);
+            Console.ReadLine();
+        }
+    }
+}
