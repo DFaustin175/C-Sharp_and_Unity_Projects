9a5b0b2e17fc6559ca24eb285555ce7c7a864afd
DFaustin175 <deefaustin175@gmail.com>
2022-03-09 11:25:17 -0500

    Main Method Calling

---
+++ Main Method Calling/Main Method Calling/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Calling
+{
+    internal class Class1
+    {
+        public static void Add(int a)
+        {
+            int a1 = a + 1;
+            Console.WriteLine(a1);
+        }
+
+        public static void Div(float b)
+        {
+            int b1 = Convert.ToInt32(b) / 2;
+            Console.WriteLine(b1);
+        }
+
+        public static void Sub(string c)
+        {
+            int c1 = Convert.ToInt32(c) - 5;
+            Console.WriteLine(c1);
+        }
+    }
+}
---
+++ Main Method Calling/Main Method Calling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Calling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter an Integer");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Class1.Add(userInput);
+            Console.ReadLine();
+            Console.WriteLine("Enter another integer");
+            float userInput2 = float.Parse(Console.ReadLine());
+            Class1.Div(userInput2);
+            Console.ReadLine();
+            Console.WriteLine("Enter one more integer");
+            string userInput3 = Console.ReadLine();
+            Class1.Sub(userInput3);
+            Console.ReadLine();
+
+        }
+    }
+}
