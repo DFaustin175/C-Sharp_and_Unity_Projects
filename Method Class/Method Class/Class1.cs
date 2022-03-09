5cb77cbeff3945fa336cabaa2553751b214e8ae3
DFaustin175 <deefaustin175@gmail.com>
2022-03-09 17:41:30 -0500

    Method Class

---
+++ Method Class/Method Class/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    internal class Class1
+    {
+        public void Math(int a, int b)
+        {
+            int a1 = a + 100;
+            Console.WriteLine(a1);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Class1 class1 = new Class1();
+            class1.Math(12, 12);
+            Console.ReadLine();
+
+        }
+    }
+}
