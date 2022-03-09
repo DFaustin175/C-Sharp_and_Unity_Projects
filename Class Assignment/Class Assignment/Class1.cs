482c9b0bc1f7501ffa0d05d4d88e7203c49f7590
DFaustin175 <deefaustin175@gmail.com>
2022-03-09 17:59:29 -0500

    ClassAssign

---
+++ Class Assignment/Class Assignment/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    internal class Class1
+    {
+        public void Div(int a)
+        {
+            float a1 = a / 2;
+            Console.WriteLine(a1);
+        }
+
+        public void Out(out int num)
+        {
+            num = 25;
+        }
+
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            return sum;
+        }
+    }
+}
---
+++ Class Assignment/Class Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            class1.Div(userInput);
+            int sum1 = class1.Add(1, 4);
+            int sum2 = class1.Add(2, 5, 7);
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.ReadLine();
+        }
+    }
+}
