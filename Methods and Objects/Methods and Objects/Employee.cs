44b195ed86ee112e37fa3d3e51d3a20ca4bc0f1e
DFaustin175 <deefaustin175@gmail.com>
2022-03-14 17:02:42 -0400

    Operator Overload

--- Methods and Objects/Methods and Objects/Employee.cs
+++ Methods and Objects/Methods and Objects/Employee.cs
@@ -8,6 +8,25 @@ namespace Methods_and_Objects
 {
     internal class Employee : Person
     {
-        public int Id = 0;
+        public int Id;
+
+       public static bool operator==(Employee employee, Employee employee2)
+        {
+            if (employee.Id == employee2.Id)
+                return true;
+            else 
+                return false;
+        }
+
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            if (employee.Id != employee2.Id)
+                return false;
+            else
+                return true;
+        }
+
+
+
     }
 }
--- Methods and Objects/Methods and Objects/Program.cs
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -14,7 +14,15 @@ namespace Methods_and_Objects
             Console.WriteLine("Please input First and Last Name");
             employee.FirstName = "Sample";
             employee.LastName = "Student";
+            employee.Id = 12;
             employee.SayName();
+
+            Employee employee2 = new Employee();
+            Console.WriteLine("Please input First and Last Name");
+            employee2.FirstName = "Sample";
+            employee2.LastName = "Student2";
+            employee.Id = 18;
+            employee2.SayName();
             Console.ReadLine();
 
         }
