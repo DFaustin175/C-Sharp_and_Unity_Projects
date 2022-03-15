c7571454d3e96d590b3f1886af507fdb32e3556c
DFaustin175 <deefaustin175@gmail.com>
2022-03-15 11:47:39 -0400

    Method And Objects Overload

--- Methods and Objects/Methods and Objects/Program.cs
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -14,15 +14,19 @@ namespace Methods_and_Objects
             Console.WriteLine("Please input First and Last Name");
             employee.FirstName = "Sample";
             employee.LastName = "Student";
-            employee.Id = 12;
+            employee.Id = 18;
             employee.SayName();
 
             Employee employee2 = new Employee();
             Console.WriteLine("Please input First and Last Name");
             employee2.FirstName = "Sample";
             employee2.LastName = "Student2";
-            employee.Id = 18;
+            employee2.Id = 18;
             employee2.SayName();
+
+            if (employee2.Id == employee.Id)
+                Console.WriteLine("Same ID");
+            else Console.WriteLine("Different ID");
             Console.ReadLine();
 
         }
