6bd544f21863ed04ad0dbefa5a9547b5320a0d50
DFaustin175 <deefaustin175@gmail.com>
2022-03-15 19:52:20 -0400

    Parameters

--- Methods and Objects/Methods and Objects/Employee.cs
+++ Methods and Objects/Methods and Objects/Employee.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace Methods_and_Objects
 {
-    internal class Employee : Person
+    internal class Employee<T> : Person
     {
         public int Id;
 
-       public static bool operator==(Employee employee, Employee employee2)
+        public int Length { get; internal set; }
+
+        public void things(T input) { }
+        
+
+       public static bool operator==(Employee<T> employee, Employee<T> employee2)
         {
             if (employee.Id == employee2.Id)
                 return true;
@@ -18,7 +23,7 @@ namespace Methods_and_Objects
                 return false;
         }
 
-        public static bool operator !=(Employee employee, Employee employee2)
+        public static bool operator !=(Employee<T> employee, Employee<T> employee2)
         {
             if (employee.Id != employee2.Id)
                 return false;
--- Methods and Objects/Methods and Objects/Program.cs
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -10,14 +10,15 @@ namespace Methods_and_Objects
     {
         static void Main()
         {
-            Employee employee = new Employee();
+            
+            Employee<string> employee = new Employee<string>();
             Console.WriteLine("Please input First and Last Name");
             employee.FirstName = "Sample";
             employee.LastName = "Student";
             employee.Id = 18;
             employee.SayName();
 
-            Employee employee2 = new Employee();
+            Employee<string> employee2 = new Employee<string>();
             Console.WriteLine("Please input First and Last Name");
             employee2.FirstName = "Sample";
             employee2.LastName = "Student2";
@@ -27,6 +28,25 @@ namespace Methods_and_Objects
             if (employee2.Id == employee.Id)
                 Console.WriteLine("Same ID");
             else Console.WriteLine("Different ID");
+            
+            Employee<string> list1 = new Employee<string>();
+            list1.things("Thing");
+            list1.things("Thing1");
+            list1.things("Thing2");
+            list1.things("Thing3");
+
+            Employee<int> list2 = new Employee<int>();
+            list2.things(1);
+            list2.things(2);
+            list2.things(3);
+            list2.things(4);
+            list2.things(5);
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                Console.WriteLine("things");
+            }
+                
             Console.ReadLine();
 
         }
