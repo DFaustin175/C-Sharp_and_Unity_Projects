c366071c2a3be0489c9154d1da7968490b20feed
DFaustin175 <deefaustin175@gmail.com>
2022-03-16 23:29:12 -0400

    Parameters

--- Methods and Objects/Methods and Objects/Program.cs
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -30,23 +30,29 @@ namespace Methods_and_Objects
             else Console.WriteLine("Different ID");
 
             Employee<string> list1 = new Employee<string>();
+            list1.things = new List<string>();
             list1.things.Add("Thing");
             list1.things.Add("Thing1");
             list1.things.Add("Thing2");
             list1.things.Add("Thing3");
 
             Employee<int> List2 = new Employee<int>();
+            List2.things = new List<int>();
             List2.things.Add(1);
             List2.things.Add(2);
             List2.things.Add(3);
             List2.things.Add(4);
             List2.things.Add(5);
 
-            for (int i = 0; i < list1.Length; i++)
+            for (int i = 0; i < list1.things.Count; i++)
             {
                 Console.WriteLine(list1.things[i]);
             }
-                
+
+            for (int i = 0; i < List2.things.Count; i++)
+            {
+                Console.WriteLine(List2.things[i]);
+            }
 
             Console.ReadLine();
 
